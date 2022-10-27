c4ae1132e0aabd71f65e19bbfafbfacccea31209
Shaqeeb Momen <shaqeebmomen@yahoo.ca>
2022-10-27 16:58:32 -0400

    Project Setup

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "hardware/uart.h"
+#include <pico/stdio_usb.h>
+
+    
+
+
+    
+    int main()
+    {
+
+        while(true)
+        {
+
+        }
+    }
+    
