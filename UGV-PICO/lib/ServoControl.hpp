9d19ef49197968fb7d7c48e1323ac3b3c4bb40b1
Shaqeeb <shaqeebmomen@yahoo.ca>
2023-04-11 01:35:56 -0400

    added degree conversion

--- UGV-PICO/lib/ServoControl.hpp
+++ UGV-PICO/lib/ServoControl.hpp
@@ -6,22 +6,26 @@
 
 // #define PIN_SERVO 22
 
-#define CLOSED_POSITION 100
-#define OPEN_POSITION 800
-
+#define CLOSED_POSITION 124378
+#define OPEN_POSITION 248756
+#define TICKS_PER_DEG 690
 
 class ServoControl
 {
 private:
     uint pin;
-    // Wrap level, indicates with single pulse ends
-    uint16_t wrapLevel = 1000u;
-    // Create constant for level
-    uint16_t compareLevel = 100; // Should be minimum pulse high width
+    // Wrap level, constant indicates with single pulse ends
+    uint16_t wrapLevel = 2487562u;
+    // Compare level
+    // uint16_t compareLevel = 100; // Should be minimum pulse high width
+    // 1-2 ms = 124378 to 248756 ticks
+    // each degree is 690 ticks for a range of 0 to 180
+    void convertDeg(uint deg);
+    void write(uint16_t level);
 
 public:
+    void writeDeg(uint deg);
     ServoControl(uint pin);
-    void write(uint16_t level);
 };
 
 ServoControl::ServoControl(uint pin)
@@ -31,7 +35,19 @@ ServoControl::ServoControl(uint pin)
 
     // Config pins
     gpio_set_function(this->pin, GPIO_FUNC_PWM);
-    write(OPEN_POSITION);
+    writeDeg(0);
+}
+
+// take in degrees and map to ticks
+void ServoControl::convertDeg(uint deg)
+{
+    // each degree is 690 ticks for a range of 0 to 180
+    uint16_t ticks = TICKS_PER_DEG * deg + CLOSED_POSITION;
+}
+
+void ServoControl::writeDeg(uint deg)
+{
+    write(convertDeg(deg));
 }
 
 // From 0-1000
@@ -39,6 +55,6 @@ void ServoControl::write(uint16_t level)
 {
     uint slice = pwm_gpio_to_slice_num(pin);
     pwm_set_wrap(slice, this->wrapLevel);
-    pwm_set_gpio_level(pin, compareLevel);
+    pwm_set_gpio_level(pin, level);
     pwm_set_enabled(slice, true);
 }
