1d4fe3c8d5a95e9e54c48184e59d1ce2b2c140c5
Shaqeeb Momen <shaqeebmomen@yahoo.ca>
2023-04-11 02:29:54 -0400

    arm test done

--- UGV-PICO/main.cpp
+++ UGV-PICO/main.cpp
@@ -178,7 +178,7 @@ auto_init_mutex(pwrMtx);
 #endif
 
 #ifdef SERVO_TEST
-uint servoSetpoint = 0;
+uint16_t servoSetpoint = 0;
 #endif
 
 // Main function to execute on core 1 (Mainly used for telemetry)
@@ -428,7 +428,7 @@ void core0_main()
 
             // robotFSMLoop();
 
-            arm->writeDeg(servoSetpoint);
+            arm->write(servoSetpoint);
 
             lastLoopTs = currentTs;
         }
