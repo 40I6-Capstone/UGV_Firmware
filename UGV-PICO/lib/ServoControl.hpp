8c69e5dcb256842353a634bc9fb5ea8276cbfe2b
Shaqeeb <shaqeebmomen@yahoo.ca>
2023-04-11 01:31:29 -0400

    convert to degrees

--- UGV-PICO/lib/ServoControl.hpp
+++ UGV-PICO/lib/ServoControl.hpp
@@ -14,7 +14,10 @@ class ServoControl
 {
 private:
     uint pin;
+    // Wrap level, indicates with single pulse ends
     uint16_t wrapLevel = 1000u;
+    // Create constant for level
+    uint16_t compareLevel = 100; // Should be minimum pulse high width
 
 public:
     ServoControl(uint pin);
@@ -36,6 +39,6 @@ void ServoControl::write(uint16_t level)
 {
     uint slice = pwm_gpio_to_slice_num(pin);
     pwm_set_wrap(slice, this->wrapLevel);
-    pwm_set_gpio_level(pin, level);
+    pwm_set_gpio_level(pin, compareLevel);
     pwm_set_enabled(slice, true);
 }
